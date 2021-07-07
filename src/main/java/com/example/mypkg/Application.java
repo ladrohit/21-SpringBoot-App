15d267cfdd5a6f24612af8154dc466f6759c81bd
Rohit <ladrohit345@gmail.com>
2021-07-07 16:44:22 +0530

    HIS-3000:Related changes

--- src/main/java/com/example/mypkg/Application.java
+++ src/main/java/com/example/mypkg/Application.java
@@ -6,6 +6,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
+	private static final String REST_URL="https:www.equifax.com/getScores";
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
