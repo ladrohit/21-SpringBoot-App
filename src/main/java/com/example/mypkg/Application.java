ce635b99c757288c8f7de5b5a8e3ebcdc1eaaa86
Rohit <ladrohit345@gmail.com>
2021-07-07 17:08:50 +0530

    HIS-301:Related changes

--- src/main/java/com/example/mypkg/Application.java
+++ src/main/java/com/example/mypkg/Application.java
@@ -6,11 +6,17 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
+	private Map<String,Object> cache= new HashMap<String,Object>()
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run=SpringApplication.run(Application.class, args);
 		//Changes for HIS-134
 		run.close();
 	}
+
+	//HIS-301 changes
+	public void loadDateToCache(){
+		//logic goes here
+}
 
 }
